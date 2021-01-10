68d43b6ef71f6233895188205d6d9e159d68731d
Nazorm <nnadoziechinazor@gmail.com>
2021-01-10 12:45:21 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import axios from 'axios'
 import Movielist from './components/Movielist';
 import './App.css';
 
 function App() {
 	const [movieResult, updateMovieResult] = useState([]);
 	const [userquery, updateUserQuery] = useState('');
+	const [loading, setLoading] = useState(false);
 
 	const handleChange = (e) => {
     const { value } = e.target;
@@ -12,13 +14,22 @@ function App() {
     
 	};
 	const handleSubmit = (e) => {
-    e.preventDefault()
-    fetch(`http://www.omdbapi.com/?s=${userquery}&apikey=${process.env.REACT_APP_API_KEY}`)
-    .then((resp)=>resp.json())
-    .then((data)=>{
-      updateMovieResult(data)
-      console.log(data, movieResult )
-    })   
+	e.preventDefault()
+	const fetchRecords = async()=>{
+  setLoading(true);
+  const res = await axios.get(`http://www.omdbapi.com/?s=${userquery}&apikey=${process.env.REACT_APP_API_KEY}`);
+  updateMovieResult(res.data.Search)
+  setLoading(false);
+  console.log(movieResult)
+}
+fetchRecords()
+
+    // fetch(`http://www.omdbapi.com/?s=${userquery}&apikey=${process.env.REACT_APP_API_KEY}`)
+    // .then((resp)=>resp.json())
+    // .then((data)=>{
+    //   updateMovieResult(data.Search)
+    //   console.log(movieResult, data )
+    // })   
 	};
 
 	return (
@@ -36,7 +47,7 @@ function App() {
 				<button onSubmit={handleSubmit}> Enter</button>
 			</form>
 
-			<Movielist movieresult={movieResult} />
+			<Movielist movieresult={[1,2,3,4,5]} />
 		</div>
 	);
 }
--- src/components/Movielist.js
+++ src/components/Movielist.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import Nominationspage from './Nominationspage'
 function Movielist(props){
-    // const movies = props.movieresult.map((d)=>{
-    //     return(
-    //         <div>
-    //             <h4>d.name</h4>
-    //         </div>
-    //     )
-    // })
+    const movies = props.movieresult.map((index, d)=>{
+        return(
+            <Nominationspage 
+            key= {index}
+            movie={d}/>
+        )
+    })
     return(
         <div>
-        
+        {movies}
         </div>
     )
 }
