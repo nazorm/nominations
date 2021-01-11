30a99fa1828c48352d73119abb74f7c4c1f0d2aa
Nazorm <nnadoziechinazor@gmail.com>
2021-01-11 06:19:59 +0100

    nominations page

--- src/components/Nominationspage.js
+++ src/components/Nominationspage.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
 
-function Nominationspage(){
+function Nominationspage(props){
+    const nominations = props.nominationresult.map((d)=>{
+        return(
+            <div className='nominated-movie' key={d.imdbID}>
+                 <img src={d.Poster} alt='poster'/>
+            <h4>Title: {d.Title}</h4>
+            <h4>Year: {d.Year}</h4>
+            <button className='remove-nomination-btn' onClick={()=>props.handleRemove(d.imdbID)}>Remove Nomination</button>
+            </div>
+        )
+    })
 return(
-    <div>
-        <h1>Nominations</h1>
+    <div className='nominated-movie'>
+        <button className='direct-to-home-btn' onClick={props.directToHome}>Home</button>
+        {nominations}
     </div>
 )
 }
