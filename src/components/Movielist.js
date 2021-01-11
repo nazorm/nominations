f5587675818e3e5225bcecc4b338b359623ed982
Nazorm <nnadoziechinazor@gmail.com>
2021-01-11 09:36:57 +0100

    home page

--- src/components/Movielist.js
+++ src/components/Movielist.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import '../App.css';
 function Movielist(props){
     const movies = props.movieresult.map(( d)=>{
         return(
@@ -6,13 +7,21 @@ function Movielist(props){
             <img src={d.Poster} alt='poster'/>
             <h4>Title: {d.Title}</h4>
             <h4>Year: {d.Year}</h4>
-            <button className='nominate-btn'onClick={()=>props.handleNomination(d.imdbID)}>Nominate</button>
+            <button className='nominate-btn movie-action-btn'
+            onClick={()=>props.handleNomination(d.imdbID)}>
+                Nominate</button>
         </div>
         )
     })
     return(
-        <div className='movies'>
-    	<form onSubmit={props.handleSubmit}>
+        <div className='home-page'>
+            <div className='movie-header home-movie-header'>
+                <div className='navigate'>
+                    <h1 className='web-name'>Shopify Movie Nominations</h1>
+                <button className='direct-to-nomination-btn header-btn'onClick={props.directToNomination}>Nominations</button>
+                </div>
+                
+    	<form className='form' onSubmit={props.handleSubmit}>
 				<input
 					type="text"
 					name="userInput"
@@ -21,10 +30,14 @@ function Movielist(props){
 					onChange={props.handleChange}
 					value={props.userquery}
 				/>
-				<button className='search-btn' onSubmit={props.handleSubmit}> Enter</button>
+				<button className='search-btn header-btn' onSubmit={props.handleSubmit}> Enter</button>
 			</form>
-            <button className='direct-to-nomination-btn'onClick={props.directToNomination}>Nomination</button>
-        {props.loading? <p>Loading...</p> : movies}
+            
+            </div>
+            <div className='movies-container'>
+            {props.loading? <p>Loading...</p> : movies}
+            </div>
+        
         </div>
     )
 }
