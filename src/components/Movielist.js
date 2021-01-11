7e89f5896a715e96c50292d3357997faf1084188
Nazorm <nnadoziechinazor@gmail.com>
2021-01-11 06:19:42 +0100

    updates home page

--- src/components/Movielist.js
+++ src/components/Movielist.js
@@ -1,16 +1,30 @@
 import React from 'react'
-import Nominationspage from './Nominationspage'
 function Movielist(props){
-    const movies = props.movieresult.map((index, d)=>{
+    const movies = props.movieresult.map(( d)=>{
         return(
-            <Nominationspage 
-            key= {index}
-            movie={d}/>
+            <div className='movie' key ={d.imdbID}>
+            <img src={d.Poster} alt='poster'/>
+            <h4>Title: {d.Title}</h4>
+            <h4>Year: {d.Year}</h4>
+            <button className='nominate-btn'onClick={()=>props.handleNomination(d.imdbID)}>Nominate</button>
+        </div>
         )
     })
     return(
-        <div>
-        {movies}
+        <div className='movies'>
+    	<form onSubmit={props.handleSubmit}>
+				<input
+					type="text"
+					name="userInput"
+					placeholder="Search Movie"
+					className="userInput"
+					onChange={props.handleChange}
+					value={props.userquery}
+				/>
+				<button className='search-btn' onSubmit={props.handleSubmit}> Enter</button>
+			</form>
+            <button className='direct-to-nomination-btn'onClick={props.directToNomination}>Nomination</button>
+        {props.loading? <p>Loading...</p> : movies}
         </div>
     )
 }
