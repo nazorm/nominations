6a10bb44690250c4df6495014d91f344ff8ddd16
Nazorm <nnadoziechinazor@gmail.com>
2021-01-11 09:37:12 +0100

    nominations page

--- src/components/Nominationspage.js
+++ src/components/Nominationspage.js
@@ -1,21 +1,29 @@
-import React from 'react'
-
+import React from 'react';
+import '../App.css';
 
 function Nominationspage(props){
     const nominations = props.nominationresult.map((d)=>{
         return(
-            <div className='nominated-movie' key={d.imdbID}>
+            <div className='movie' key={d.imdbID}>
                  <img src={d.Poster} alt='poster'/>
             <h4>Title: {d.Title}</h4>
             <h4>Year: {d.Year}</h4>
-            <button className='remove-nomination-btn' onClick={()=>props.handleRemove(d.imdbID)}>Remove Nomination</button>
+            <button className='remove-nomination-btn movie-action-btn' onClick={()=>props.handleRemove(d.imdbID)}>Remove Nomination</button>
             </div>
         )
     })
 return(
-    <div className='nominated-movie'>
-        <button className='direct-to-home-btn' onClick={props.directToHome}>Home</button>
+    <div className='nominationspage'>
+        <div className='navigate'>
+        <h1 className='web-name'>Shopify Movie Nominations</h1>
+        <button className='direct-to-home-btn header-btn' onClick={props.directToHome}>Home</button>
+        </div>
+       
+        
+        <div className='movies-container'>
         {nominations}
+        </div>
+        
     </div>
 )
 }
